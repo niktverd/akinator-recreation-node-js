2137affd85c7fad50098d8a9885e65c35e128cfb
Nik Tverd <nn.tverd@gmail.com>
2024-01-11 08:58:28 +0600

    feat: db connection arranged

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,12 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
     client: 'postgresql',
     connection: {
-      database: 'akinator-db',
-      user:     'new_user',
-      password: '2030'
+      database: process.env.NEXT_PUBLIC_DB,
+      user:     process.env.NEXT_PUBLIC_DB_USER,
+      password: process.env.NEXT_PUBLIC_DB_PASSWORD,
     },
     pool: {
       min: 2,
@@ -24,9 +23,9 @@ module.exports = {
   staging: {
     client: 'postgresql',
     connection: {
-      database: 'akinator-db',
-      user:     'new_user',
-      password: '2030'
+      database: process.env.NEXT_PUBLIC_DB,
+      user:     process.env.NEXT_PUBLIC_DB_USER,
+      password: process.env.NEXT_PUBLIC_DB_PASSWORD,
     },
     pool: {
       min: 2,
@@ -40,17 +39,24 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'akinator-db',
-      user:     'new_user',
-      password: '2030'
+      // host: 'ec2-3-232-218-211.compute-1.amazonaws.com',
+      // database: 'd6toslorh93gv',
+      // user:     'pjvfmnubrjlcml',
+      // password: '336cd86930ca7e68de362912c2f0e0d3de9f5db8d7f9661c78c9a0f261ac0741'
+      connectionString: process.env.NEXT_PUBLIC_DB_URI,
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: 'knex_migrations',
+      directory: __dirname + "/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/seeds",
+    },
   }
 
 };
--- src/db/Game.ts
+++ src/db/Game.ts
@@ -2,8 +2,9 @@ import { Model } from 'objection';
 import Knex from 'knex';
 import knexConfig from '../../knexfile';
 
+const env = process.env.NEXT_PUBLIC_APP_ENV || 'development'
 // Инициализация knex.
-const knex = Knex(knexConfig.development);
+const knex = Knex(knexConfig[env]);
 Model.knex(knex);
 
 class Game extends Model {
